89762be021498ba25a71414b99b801ff80f5d98b
skmara2 <skmara2@inbox.lv>
2020-01-10 11:53:59 +0200

    renderēts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask('app')
@@ -6,7 +6,7 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return "Sveiciens no StartIT! VVV"
+  return render_template('index.html')
 
 
 if __name__ == '__main__':
