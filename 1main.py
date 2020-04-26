f6dde13aca9c9b3df065cb5480b2e3474ac15c43
skmara2 <skmara2@inbox.lv>
2020-04-26 22:11:02 +0300

    Update 1main.py

--- 1main.py
+++ 1main.py
@@ -1,8 +1,17 @@
+import os
 from flask import Flask, render_template, json, jsonify, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('app')
+app.config['SQLALCHEMY_DATABSE_URL'] = os.environ['DATABSE_URL']
+db=SQLAlchemy(app)
 
+class test(db.Model):
+  col=db.Column(db.String(255), primary_key=True)
+  col=db.Column(db.String(255), unique=True, nullable=False)
+  
+  def __repr__(self):
+    return '%r' % self.col
 
 @app.route('/')
 def index_page():
@@ -27,7 +36,10 @@ def suuti_zinu():
      f.write(dati["chats"]+"\n")
   return ielasit_chatu()
     
-
+@app.route('/postgreSQL')
+def postgresSQL():
+  result=test.query.all()
+  return '%r' % result
 
 
 if __name__ == '__main__':
