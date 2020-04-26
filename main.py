9e4b0e9b5268fb524f66e51831fd0728136207d6
skmara2 <skmara2@inbox.lv>
2020-04-26 21:41:03 +0300

    Update and rename otrsmain.py to main.py

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-import os
 from flask import Flask, render_template, json, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask('app')
-app.config['SQLALCHEMY_DATABASE_URL']=os.environ['DATABASE_URL']
-db=SQLAlchemy(app)
 
-class test(db.Model):
-  col=db.Column(db.String(255), primary_key=True)
-  col2=db.Column(db.String(255), unique=True, nullable=False)
-  
-  def__repr__(self):
-    return '%r' % self.col
-  
-@app.route('postgreSQL')
-def postgresSQL():
-  result=test.query.all()
-  return '%r' % result
 
 @app.route('/')
 def index_page():
@@ -47,4 +33,5 @@ def suuti_zinu():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
+
 
