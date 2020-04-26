eb08d68398ea07e1446a3888f5fea2f17e740475
skmara2 <skmara2@inbox.lv>
2020-04-26 21:25:56 +0300

    Update and rename oldmain.py to main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import os
 from flask import Flask, render_template, json, jsonify, request
-
+from flask_sqlalchemy importSQLAlchemy
 
 app = Flask('app')
+app.config['SQLALCHEMY_DATABASE_URL']=os.environ['DATABASE_URL']
+db=SQLAlchemy(app)
 
+class test(db.Model):
+  col=db.Column(db.String(255), primary_key=True)
+  col2=db.Column(db.String(255), unique=True, nullable=False)
+  
+  def__repr__(self):
+    return '%r' % self.col
+  
+@app.route('postgreSQL')
+def postgresSQL():
+  result=test.query.all()
+  return '%r' % result
 
 @app.route('/')
 def index_page():
